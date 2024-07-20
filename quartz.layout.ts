5f0aaa54bb792ad28b6e1ed1d8edeb0f02bba3fa
rafifazzaki <59862154+rafifazzaki@users.noreply.github.com>
2024-07-20 10:22:07 +0700

    Quartz sync: Jul 20, 2024, 10:22 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -17,6 +17,8 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
+    Component.Search(),
+    Component.Darkmode(),
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
@@ -29,8 +31,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Search(),
-    Component.Darkmode(),
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
